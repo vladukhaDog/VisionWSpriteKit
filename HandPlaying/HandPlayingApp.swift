cbf0ccb50809c68ceb2a4ceeee9cea0ba57421ee
vladukhaDog <vladic.p@gmail.com>
2023-01-18 09:08:01 +0300

    Initial Commit

---
+++ HandPlaying/HandPlayingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandPlayingApp.swift
+//  HandPlaying
+//
+//  Created by Permyakov Vladislav on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct HandPlayingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
